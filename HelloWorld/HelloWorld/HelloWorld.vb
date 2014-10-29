d8a0d388d0196dd2fe25e238bd249dc3b2f1d52d
andymackieM1 <andy.mackie@metaone.co.uk>
2014-10-29 14:54:12 +0000

    Start of new project

---
+++ HelloWorld/HelloWorld/HelloWorld.vb
@@ -0,0 +1,16 @@
+﻿Public Class HelloWorld
+    Private Sub HelloWorld_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        With Me
+            .Text = "Hello World"
+            .cmdHelloWorld.Text = .Text
+        End With
+
+    End Sub
+
+    Private Sub cmdHelloWorld_Click(sender As Object, e As EventArgs) Handles cmdHelloWorld.Click
+
+        MessageBox.Show(Me.Text, Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+
+    End Sub
+End Class
